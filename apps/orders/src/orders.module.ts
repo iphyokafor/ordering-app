c56e1114e8adb3f505177261160fa8ad1d779b9c
iphyokafor <sandraifeoma22@gmail.com>
2022-12-13 23:19:26 +0100

    Setup common libraries

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
+import * as Joi from 'joi'
+import { DatabaseModule } from '@app/common';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required()
+      }),
+      envFilePath: './apps/orders/.env'
+    }),
+
+    DatabaseModule
+  ],
+  
   controllers: [OrdersController],
   providers: [OrdersService],
 })
